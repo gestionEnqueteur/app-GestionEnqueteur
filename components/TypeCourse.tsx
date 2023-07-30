e58324368841622b428b2f62bf79f8a41fa893a6
jeanCodeUnMax <webmanhtc@gmail.com>
2023-07-30 20:28:40 +0200

    :lipstick: Creation du composant TypeCourse

---
+++ components/TypeCourse.tsx
@@ -0,0 +1,36 @@
+
+import { View, Text } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+type Props = {
+  mission: string;
+};
+
+export default function TypeCourse({ mission }: Props) {
+
+  let icon: string;
+
+  switch (mission) {
+    case "BSC HDF":
+      icon = "train";
+      break;
+    case "MQ HDF":
+      icon = "bus";
+      break;
+    case "HLP Train":
+      icon = "sleep";
+      break;
+    case "HLP VS":
+      icon = "car";
+      break;
+    default:
+      icon = "";
+      break;
+  }
+
+  return (
+    <View>
+      <MaterialCommunityIcons name={icon} color="black" size={26} />
+    </View>
+  );
+}
