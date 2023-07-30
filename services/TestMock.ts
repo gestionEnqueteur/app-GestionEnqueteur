0642a08907c888715600f9d4545ce9a7b0ac2083
Rémi PONCHE <ponche62880@gmail.com>
2023-07-31 00:13:04 +0200

    🤡 Création du service TestMock

---
+++ services/TestMock.ts
@@ -0,0 +1,35 @@
+import planning from "../mock/planning.json";
+import Course from "../models/Course";
+import { StatusEnum } from "../models/enum";
+
+export default class TestMock {
+  static getCourses(): Course[] {
+    let count = 1;
+    const listCourse = planning.map((coursePlanning) => {
+      const course: Course = {
+        id: count,
+        mission: coursePlanning.mission,
+        pds: coursePlanning.pds,
+        vac: coursePlanning.vac,
+        affectation: coursePlanning.affectation,
+        status: StatusEnum.DRAFT,
+        ligne: coursePlanning.ligne,
+        trainCourse: coursePlanning.trainCourse,
+        objectif: coursePlanning.prQuota,
+        infoHoraireCourse: {
+          datetimeArriveEnq: new Date(coursePlanning.ha),
+          datetimeDepartEnq: new Date(coursePlanning.hd),
+          gareDepartEnq: coursePlanning.depart,
+          gareArriveEnq: coursePlanning.arrivee,
+        },
+      };
+
+      // incrementer mon compteur
+      count++;
+      //
+      return course;
+    });
+
+    return listCourse;
+  }
+}
