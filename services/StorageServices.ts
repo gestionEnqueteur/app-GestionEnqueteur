b364bfab3df733e62db69d4bc883f502566108f6
Rémi PONCHE <ponche62880@gmail.com>
2023-08-05 15:19:00 +0200

    🌱 Ajout un début de services:  Storage et Axios
    création des fichiers de départ pour les services Storage et Axios.

---
+++ services/StorageServices.ts
@@ -0,0 +1,15 @@
+import Course from "../models/Course";
+
+export default class StorageService {
+  static saveCourse(course: Course) {
+    //TODO: crée la méthode pour sauvergader une course.
+  }
+
+  static getCourse() {
+    //TODO: crée la méthode pour récuperer une course. dans la database.
+  }
+
+  static getAllCourse() {
+    //TODO: crée la méthode pour récuperer tout les courses dans la database.
+  }
+}
