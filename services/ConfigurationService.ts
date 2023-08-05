8004fcf981d4144dcf3e800dd7ed5536ce0d6193
Rémi PONCHE <ponche62880@gmail.com>
2023-08-05 15:52:46 +0200

    :seedling: Crée les fichiers de départ des services
    crée les services de départ de Course et Configuration service

--- services/ConfigurationService.ts
+++ services/ConfigurationService.ts
@@ -1,6 +1,39 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import ConfigurationType from "../models/ConfigurationType";
 
+export default class ConfigurationService {
+  private configuration: ConfigurationType;
+
+  constructor(configuration: ConfigurationType) {
+    this.configuration = configuration;
   }
 
-//TODO: rajouter la fonction setConfiguration
+  async loadConfiguration() {
+    try {
+      const jsonValue = await AsyncStorage.getItem("configuration");
+      const value = jsonValue != null ? JSON.parse(jsonValue) : null;
+      if (value) this.configuration = value;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async saveConfiguration(newConfiguration: ConfigurationType) {
+    try {
+      const jsonValue = JSON.stringify(newConfiguration);
+      await AsyncStorage.setItem("configuration", jsonValue);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  getConfiguration() {
+    return this.configuration;
+  }
+
+  setConfiguration(newConfiguration: ConfigurationType) {
+    //TODO: mettre en place la sécurité ici
+
+    this.saveConfiguration(newConfiguration);
+  }
+}
--- services/CourseService.ts
+++ services/CourseService.ts
@@ -2,6 +2,25 @@ import axios from "axios";
 
 import Course from "../models/Course";
 import TestMock from "./TestMock";
+import ConfigurationType from "../models/ConfigurationType";
 
+export default class CourseService {
+  private configuration: ConfigurationType;
+  private courses: Course[];
+
+  constructor(configuration: ConfigurationType) {
+    this.configuration = configuration;
+    this.courses = [];
+  }
+
+  getCourses() {
+    return this.courses;
+  }
+
+  updateCourse(id: number, course: Course) {
+    //TODO: update la course.
+    // récupérer la course en fonction de ID de l'objet.
+    // changer  la valeur du tableau.
+    // enregistrer dans la Db du téléphone
   }
 }
