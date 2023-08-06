a5c7095efb0cec1420d4b2775cbe58ac1261f1f2
Rémi PONCHE <ponche62880@gmail.com>
2023-08-06 10:11:32 +0200

    👔 Création du service ConfigurationService

--- services/ConfigurationService.ts
+++ services/ConfigurationService.ts
@@ -1,39 +1,56 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import ConfigurationType from "../models/ConfigurationType";
 
-export default class ConfigurationService {
-  private configuration: ConfigurationType;
+export default class ConfigurationService implements ConfigurationType {
+  urlApi: string;
+  user: string;
 
   constructor(configuration: ConfigurationType) {
-    this.configuration = configuration;
+    this.urlApi = configuration.urlApi;
+    this.user = configuration.user;
   }
 
   async loadConfiguration() {
+    console.log("chargement de la config dans AsyncStorage");
+
     try {
       const jsonValue = await AsyncStorage.getItem("configuration");
       const value = jsonValue != null ? JSON.parse(jsonValue) : null;
-      if (value) this.configuration = value;
+      if (value) {
+        this.urlApi = value.urlApi;
+        this.user = value.user;
+      }
     } catch (e) {
       console.log(e);
     }
   }
 
-  async saveConfiguration(newConfiguration: ConfigurationType) {
+  async saveConfiguration(
+    newConfiguration: ConfigurationType,
+    succesCallback?: Function,
+    failureCallback?: Function
+  ) {
+    console.log("save la configuration");
     try {
       const jsonValue = JSON.stringify(newConfiguration);
       await AsyncStorage.setItem("configuration", jsonValue);
+      this.urlApi = newConfiguration.urlApi;
+      this.user = newConfiguration.user;
+      if (succesCallback) succesCallback();
     } catch (e) {
       console.error(e);
+      if (failureCallback) failureCallback(e);
     }
   }
 
   getConfiguration() {
-    return this.configuration;
+    return {urlApi: this.urlApi, user: this.user};
   }
 
   setConfiguration(newConfiguration: ConfigurationType) {
     //TODO: mettre en place la sécurité ici
 
     this.saveConfiguration(newConfiguration);
+    return this;
   }
 }
