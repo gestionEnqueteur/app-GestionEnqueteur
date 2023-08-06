c31dcf904f1242fb1882ce8bfac5bef0720f8693
Rémi PONCHE <ponche62880@gmail.com>
2023-08-06 10:31:52 +0200

    ✨ Ajout le refresh par action manuel du client
    les courses peuvent etre actualiser en tirant avec le doigts du haut en bas.

--- pages/TrainPlanifieScreen.tsx
+++ pages/TrainPlanifieScreen.tsx
@@ -1,19 +1,43 @@
-import { StyleSheet, ScrollView } from "react-native";
+import { StyleSheet, FlatList } from "react-native";
 import DetailCourse from "../components/DetailCourse";
-import { useEffect } from "react";
-
-import { Text } from "react-native-paper";
+import { useContext, useEffect, useState } from "react";
+import { CourseContext } from "../provider/AppProvider";
+import Course from "../models/Course";
 
 export default function TrainPlanifieScreen() {
+  const courseService = useContext(CourseContext);
+
+  const [courses, setCourses] = useState<Course[]>([]);
+
   useEffect(() => {
     // init du composant.
     console.log("Mount: train");
+    if (courseService) {
+      const loadedCourses = courseService.getCourses();
+      console.log(loadedCourses);
+      setCourses(loadedCourses);
+    }
   }, []);
 
+  const renderItem = ({ item }: { item: Course }) => (
+    <DetailCourse course={item} />
+  );
+
+  const handleOnRefresh = () => {
+    if (courseService) {
+      setCourses(courseService.loadCourses());
+      console.log("refresh");
+    }
+  };
+
   return (
-    <ScrollView style={style.container}>
-      <Text>Test de la pages</Text>
-    </ScrollView>
+    <FlatList
+      data={courses}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id.toString()}
+      onRefresh={handleOnRefresh}
+      refreshing={false}
+    />
   );
 }
 
