e15ca66d758cbb4d43c04144803b7a41ddef348b
Rémi PONCHE <ponche62880@gmail.com>
2023-08-06 09:23:25 +0200

    🚧 Début de fonction loadCourses pour test
    utilisation de fichier minimal pour tester le concept de mon idée.

--- services/CourseService.ts
+++ services/CourseService.ts
@@ -5,7 +5,7 @@ export default class CourseService {
   private storage: StorageService;
   private courses: Course[];
 
-  constructor( storage: StorageService) {
+  constructor(storage: StorageService) {
     this.storage = storage;
     this.courses = [];
   }
@@ -19,5 +19,13 @@ export default class CourseService {
     // récupérer la course en fonction de ID de l'objet.
     // changer  la valeur du tableau.
     // enregistrer dans la Db du téléphone
+  }
+
+  loadCourses() {
+    //TODO: implémenter la fonction
+    // on tente un Fetch, si pas de 4G (on récupere sur la db Téléphone )
+    console.log("Chargement des courses depuis Storage Service");
+    this.courses = this.storage.getAllCourse();
+    return this.courses;
   }
 }
