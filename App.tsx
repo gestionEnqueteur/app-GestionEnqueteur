c37fa1c12812cefa5710e1786ad57a878ee39a68
Rémi PONCHE <ponche62880@gmail.com>
2023-08-06 10:26:28 +0200

    ⚰️ Retire les import inutiles dans App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { PaperProvider } from "react-native-paper";
-import ConfigurationProvider from "./provider/ConfigurationProvider";
 import MyMainStack from "./pages/navigations/StackNavigation";
-import CourseProvider from "./provider/CourseProvider";
 import AppProvider from "./provider/AppProvider";
 
 export default function App() {
