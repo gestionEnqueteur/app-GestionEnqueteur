349e49cded62eb4d2472ff9c2eb168e815a62e0d
Rémi PONCHE <ponche62880@gmail.com>
2023-08-07 10:36:51 +0200

    🌱 Préparation fonctions pour StorageService

--- services/StorageServices.ts
+++ services/StorageServices.ts
@@ -2,7 +2,17 @@ import Course from "../models/Course";
 import TestMock from "./TestMock";
 
 export default class StorageService {
+  async getData(entity: string) {
+    //TODO: faire la fonction getData
   }
+  async postData(entity: string, data: any) {
+    //TODO: faire la fonction postData
+  }
+  async updateData(entity: string, id: number, data: any) {
+    //TODO: faire la fonction updateData
+  }
+  async deleteData(entity: string, id: number) {
+    //TODO: faire la fonction deleteData
   }
 
   getAllCourse() {
