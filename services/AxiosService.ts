ae9d00ddf6aa2e29b8581ccac1239f463475069b
Ulysse <u.hischke@outlook.fr>
2023-08-21 21:36:43 +0200

    :construction: Creation de fonction private pour le bon fonctionnement du service Axios + fonct GetData

--- services/AxiosService.ts
+++ services/AxiosService.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from "axios";
+import { log } from "console";
 
 export default class AxiosService {
   private urlApi: string;
@@ -7,22 +8,53 @@ export default class AxiosService {
   constructor(urlApi: string) {
     this.urlApi = urlApi;
     this.instanceAxios = axios.create({
-      baseURL: urlApi,
+      baseURL: this.urlApi,
     });
   }
-  setUrlApi(urlApi: string) {
-    this.urlApi = urlApi;
 
-    return this;
+  pingService(): void {
+    // TODO : Ajouter une route strapi quie permet de ping le back pour savoir si il est up
+    console.log(
+      `Service Axios: is running on port -> ${
+        this.urlApi
+      }, the instance BaseUrl is ${this.instanceAxios.getUri()}`
+    );
   }
 
-  getInstance() {
+  private setNewAxiosInstance(url: string): void {
+    this.instanceAxios = axios.create({
+      baseURL: this.urlApi,
+    });
+  }
+
+  setUrlApi(urlApi: string): void {
+    this.urlApi = urlApi;
+    this.setNewAxiosInstance(this.urlApi);
+  }
+
+  private getInstance(): AxiosInstance {
     return this.instanceAxios;
   }
 
-  async getData(path: string) {
-    //TODO: faire la fonction getData
+  private checkValidUrl(axiosInstance: AxiosInstance): void {
+    if (!axiosInstance.getUri()) {
+      console.log("Pas d'url saisi");
+      throw new Error("Url introuvable");
+    }
+    // TODO: ping sur l'api pour savoir sil elle est up ou joignable sinon renvoie une error
   }
+
+  async getData(path: string): Promise<any> {
+    const axiosInstance = this.getInstance();
+    this.checkValidUrl(axiosInstance);
+    try {
+      const res = await axiosInstance.get(path);
+      return res;
+    } catch (err) {
+      console.log("Error sur la requete GET :", err);
+    }
+  }
+
   async postData(path: string, data: any) {
     //TODO: faire la fonction postData
   }
