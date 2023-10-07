26f7d5018352716d0fa74a747159ef4518771f7a
Rémi PONCHE <ponche62880@gmail.com>
2023-10-07 11:32:35 +0200

    🚧 Brouilloin en cours
    des expérimentation SQLite

--- pages/SaisiScreen.tsx
+++ pages/SaisiScreen.tsx
@@ -1,12 +1,19 @@
 import { Text, Button } from "react-native-paper";
 import { View, StyleSheet } from "react-native"; 
+import { useContext } from "react";
+import StorageService from "../services/StorageServices";
+import { StorageContext } from "../provider/AppProvider";
 
 
 export default function SaisiScreen() {
+
+  const storage = useContext(StorageContext);
+
+
   return (
     <View style={styles.container}>
-      <Button mode="contained" onPress={() => console.log("saisi BSC")}>BSC</Button>
-      <Button mode="contained" onPress={() => console.log("saisi MQ")}>MQ</Button>
+      <Button mode="contained" onPress={() => storage?.testRequest()}>BSC</Button>
+      <Button mode="contained" onPress={() => storage?.createDatabase()}>MQ</Button>
       <Button mode="contained" onPress={() => console.log("saisi autres")}>Autres</Button>
     </View>
   )
--- services/StorageServices.ts
+++ services/StorageServices.ts
@@ -1,9 +1,34 @@
+import { WebSQLDatabase, openDatabase } from "expo-sqlite";
 import TestMock from "./TestMock";
 
 export default class StorageService {
-  async getData(entity: string) {
-    //TODO: faire la fonction getData
+  private db: WebSQLDatabase | null;
+  private fileSqlite: string;
+
+  constructor() {
+    this.fileSqlite = "db";
+    this.db = openDatabase(
+      this.fileSqlite,
+      "V1",
+      "storage téléphone",
+      undefined,
+      (db) => {
+        console.log("ouverture DB");
+        this.db = db;
+      }
+    );
   }
+
+  testRequest() {
+    this.db?.exec([{ sql: "SELECT 2 + 5, 8", args: [] }], false, (error, result) => {
+      console.log("execution request SQL");
+      console.log(result[0].rows);
+    });
+  }
+
+ 
+
+  async getData(entity: string) {}
   async postData(entity: string, data: any) {
     //TODO: faire la fonction postData
   }
@@ -12,6 +37,27 @@ export default class StorageService {
   }
   async deleteData(entity: string, id: number) {
     //TODO: faire la fonction deleteData
+  }
+
+  async createDatabase() {
+    //TODO: création de la BDD
+    this.db?.exec([{sql: `CREATE TABLE course (
+      id INT PRIMARY KEY NOT NULL, 
+      mission VARCHAR(255), 
+      pds VARCHAR(255), 
+      vac VARCHAR(255), 
+      affectation VARCHAR(255),
+      status VARCHAR(255), 
+      ligne VARCHAR(255),
+      service VARCHAR(255), 
+      objectif INT, 
+      commentaire TEXT
+    );`, args: []}], false, (error, result) => {
+      console.log(result);
+    })
+  }
+  dropDatabase() {
+    //TODO: supression de la BDD
   }
 
   /**
