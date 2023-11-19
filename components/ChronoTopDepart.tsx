2fb60d3ffc70696232a83a86eb0f5bb2fcb6dec6
Rémi PONCHE <ponche62880@gmail.com>
2023-11-19 12:43:51 +0100

    ♻️ Refactor le Chrono
    Refactor le composant ChronoTopDepart

--- components/ChronoTopDepart.tsx
+++ components/ChronoTopDepart.tsx
@@ -1,107 +1,75 @@
-import { View, Text, StyleSheet } from "react-native";
 import { useEffect, useState } from "react";
 import { Avatar } from "react-native-paper";
 import { calculDifferenceTime } from "../helpers/timeHelper";
 
-enum FormatEnum {
-  DATE,
-  BEFORE_DEPARTURE,
-  RUNNING_TRAIN,
-}
-
-enum ModeEnum {
-  BEFORE_DEPARTURE,
-  RUNNING_TRAIN,
-  DATE_DISPLAY,
-  AUTO,
-}
 
 type Props = {
   depart: Date;
   arrival: Date;
 };
 
-type State = {
-  output: string;
-  format: FormatEnum;
-  mode: ModeEnum;
-  delta: Date;
-};
-
 export default function ChronoTopDepart(props: Readonly<Props>) {
   const { depart, arrival } = props;
 
   // state global
-  const [state, setState] = useState<State>({
-    output: "init",
-    format: FormatEnum.DATE,
-    mode: ModeEnum.AUTO,
-    delta: new Date(),
-  });
+  const [output, setOutput] = useState("init");
 
-  const sixtyMinuteBeforeDeparture = () => {
+  const sixtyMinuteBeforeDeparture = (currentTime: Date) => {
     // 60 minutes avant le départ
-    const deltaBeforeDeparture = calculDifferenceTime(
-      new Date(),
-      depart
-    );
-    setState({
-      ...state,
-      delta: deltaBeforeDeparture,
-      output: `${deltaBeforeDeparture.getMinutes()} min`,
-    });
+    const deltaBeforeDeparture = calculDifferenceTime(currentTime, depart);
+    setOutput(`${deltaBeforeDeparture.getMinutes()} min`);
+  };
 
-  }
+  const timeBeforeArrival = (currentTime: Date) => {
+    const deltaBeforeArrival = calculDifferenceTime(currentTime, arrival);
 
-  const timeBeforeArrival = () => {
-    const deltaBeforeArrival = calculDifferenceTime(
-      new Date(),
-      arrival
-    );
+    setOutput(`${deltaBeforeArrival.getHours()}:${deltaBeforeArrival.getMinutes()}`);
+  };
 
-    setState({
-      ...state,
-      delta: deltaBeforeArrival, 
-      output: `${deltaBeforeArrival.getHours()}:${deltaBeforeArrival.getMinutes()}`,
-    }); 
-  }
-
-    const displayDate = () => {
-      setState({
-        ...state, 
-        output: `${depart.getDate()}/${depart.getMonth() + 1}`
-      }); 
-    }
+  const displayDate = () => {
+    setOutput(`${depart.getDate()}/${depart.getMonth() + 1}`);
+  };
 
   const tick = () => {
     // fonction executer tous les secondes.
+    const currentTime = new Date();
+    const ONE_HOUR = new Date(0, 0, 0, 1);
+    const oneHoureBeforeDeparture = new Date(
+      depart.getTime() - ONE_HOUR.getTime()
+    );
 
-    // vérification du mode 
-
-
-
-    //sixtyMinuteBeforeDeparture(); 
-    //timeBeforeArrival(); 
-
-    displayDate(); 
-
+    // vérification du mode
+    if (
+      currentTime.getTime() > oneHoureBeforeDeparture.getTime() &&
+      currentTime.getTime() < depart.getTime()
+    ) {
+      // on affiche le chrono avant le départ.
+      sixtyMinuteBeforeDeparture(currentTime);
+    } else if (
+      currentTime.getTime() > depart.getTime() &&
+      currentTime.getTime() <= arrival.getTime()
+    ) {
+      // on affiche le temps restant avant arriver
+      timeBeforeArrival(currentTime);
+    } else {
+      // on affiche la date
+      displayDate();
+    }
   };
 
   useEffect(() => {
     // iniT
 
     // mise en place du chrono.
-    console.log("init");
     const intervalIdenfier = setInterval(tick, 1000);
 
     return () => {
       // suppression du setInterval
       clearInterval(intervalIdenfier);
-      console.log("suppression");
     };
   }, []);
 
   return (
-    <Avatar.Text label={state.output} size={64} labelStyle={{ fontSize: 16 }} />
+    <Avatar.Text label={output} size={64} labelStyle={{ fontSize: 16 }} />
   );
 }
