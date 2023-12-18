535e8d09e710ee4b92cc2b2fc16fa773033b7dd2
Rémi PONCHE <ponche62880@gmail.com>
2023-12-18 13:59:41 +0100

    ♻️ Ajout du provider storeCourse et petit refactor
    ajout du provider storeCourse pour la gestion du state global et modification des provider pour évier le cas de context pouvant etre undefined

--- provider/AppProvider.tsx
+++ provider/AppProvider.tsx
@@ -1,64 +1,72 @@
-import {
-  ReactNode,
-  createContext,
-  useEffect,
-  useMemo,
-} from "react";
+import { ReactNode, createContext, useReducer, Dispatch, useMemo } from "react";
 import ConfigurationService from "../services/ConfigurationService";
 import AxiosService from "../services/AxiosService";
 import StorageService from "../services/StorageServices";
 import CourseService from "../services/CourseService";
+import Course from "../models/Course";
+import reducerCourse, { ActionCourse } from "../reducer/courseReducer";
 
 type Props = {
   children: ReactNode;
 };
 
+type StoreContextType = {
+  state: Course[];
+  dispatch: Dispatch<ActionCourse>;
+};
+
+// instance les services
+const configService = new ConfigurationService({ urlApi: "", user: "" });
+const axiosService = new AxiosService("");
+const storageService = new StorageService();
+const courseService = new CourseService(storageService);
+
+// création du state global
+const initialState: Course[] = [];
+export const StoreContext = createContext<StoreContextType>({
+  state: initialState,
+  dispatch: () => {},
+});
+
 // création des context pour chaque service
-export const ConfigurationContext = createContext<
-  ConfigurationService | undefined
->(undefined);
-export const AxiosContext = createContext<AxiosService | undefined>(undefined);
-export const StorageContext = createContext<StorageService | undefined>(
-  undefined
-);
-export const CourseContext = createContext<CourseService | undefined>(
-  undefined
-);
+export const ConfigurationContext =
+  createContext<ConfigurationService>(configService);
+export const AxiosContext = createContext(axiosService);
+export const StorageContext = createContext<StorageService>(storageService);
+export const CourseContext = createContext<CourseService>(courseService);
+
+// Init de l'application
+const init = async () => {
+  console.log("Init App");
+  // Chargement la configuration dans le asyncStorage
+  await configService.loadConfiguration();
+  console.log(`urlApi: ${configService.getConfiguration().urlApi}`); // pour le test à supprimer
+  console.log(`user: ${configService.getConfiguration().user}`); // pour le test à supprimer
+  // Load les courses
+  courseService.loadCourses();
+};
+
+init();
 
 export default function AppProvider(props: Readonly<Props>) {
-  // création de l'instance Configuration service
+  // Composant AppProvider
 
-  const configService = useMemo(() =>new ConfigurationService({ urlApi: "", user: "" }), []);
-  const axiosService = useMemo(() => new AxiosService(""), []);
-  const storageService = useMemo(() => new StorageService(), []);
-  const courseService = useMemo(() => new CourseService(storageService), []);
+  // le Reducer
+  const [state, dispatch] = useReducer(reducerCourse, initialState);
 
-  const init = async () => {
-    console.log("Init App");
-    // Chargement la configuration dans le asyncStorage
-    await configService.loadConfiguration();
-    console.log(`urlApi: ${configService.getConfiguration().urlApi}`);
-    console.log(`user: ${configService.getConfiguration().user}`);
-    // Load les courses
-    courseService.loadCourses(); 
-  };
-
-  useEffect(() => {
-    // Init de l'application
-    console.log("Mount: AppProvider");
-
-    init();
-  }, []);
+  const storeContextValue = useMemo(() => ({state, dispatch}), [state] )
 
   return (
-    <ConfigurationContext.Provider value={configService}>
-      <AxiosContext.Provider value={axiosService}>
-        <StorageContext.Provider value={storageService}>
-          <CourseContext.Provider value={courseService}>
-            {props.children}
-          </CourseContext.Provider>
-        </StorageContext.Provider>
-      </AxiosContext.Provider>
-    </ConfigurationContext.Provider>
+    <StoreContext.Provider value={storeContextValue}>
+      <ConfigurationContext.Provider value={configService}>
+        <AxiosContext.Provider value={axiosService}>
+          <StorageContext.Provider value={storageService}>
+            <CourseContext.Provider value={courseService}>
+              {props.children}
+            </CourseContext.Provider>
+          </StorageContext.Provider>
+        </AxiosContext.Provider>
+      </ConfigurationContext.Provider>
+    </StoreContext.Provider>
   );
 }
