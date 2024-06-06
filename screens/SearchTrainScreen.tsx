462f4dffae83b1cbc02a5b9a8bc769c090b0f293
jeanCodeUnMax <webmanhtc@gmail.com>
2024-06-07 00:58:17 +0200

    :lipstick: Mise a jour du style de searchTrainScreen

--- screens/SearchTrainScreen.tsx
+++ screens/SearchTrainScreen.tsx
@@ -1,82 +1,57 @@
 import { useState } from "react";
-import { View, StyleSheet, Switch } from "react-native";
-import { Text, TextInput, Button } from "react-native-paper";
+import { View, StyleSheet, Switch, Text } from "react-native";
+import { TextInput, Button } from "react-native-paper";
 
-const CustomInput = ({
-  label,
-  placeholder,
-  value,
-  onTextChange,
-}: {
-  label: string;
-  placeholder: string;
-  value: string;
-  onTextChange: (text: string) => void;
-}) => (
-  <View>
-    <Text style={styles.label}>{label}</Text>
-    <TextInput
-      mode="outlined"
-      placeholder={placeholder}
-      value={value}
-      onChangeText={onTextChange} // Call the onTextChange callback function
-    />
-  </View>
-);
-
-const SearchTrain = () => {
+export default function SearchTrainScreen() {
   const [departureStation, setDepartureStation] = useState("");
   const [arrivalStation, setArrivalStation] = useState("");
-  const [serviceNumber, setServiceNumber] = useState("");
+  const [numberCirculation, setNumberCirculation] = useState("");
+  const [isModeSearchNumberTrain, setIsModeSearchNumberTrain] = useState(false);
 
-  const [searchType, setSearchType] = useState(0); // 0 for station, 1 for circulation number
-
-  const toggleSearchType = () => {
-    setSearchType((prevSearchType) => (prevSearchType === 0 ? 1 : 0)); // loop on slider
+  const toggleModeSearch = () => {
+    setIsModeSearchNumberTrain((prevMode) => !prevMode);
   };
 
   const handleSearch = () => {
-    // todo Logic for search
-    console.log("Search executed");
-    console.log("Departure Station:", departureStation);
-    console.log("Arrival Station:", arrivalStation);
-    console.log("serviceNumber:", serviceNumber);
-
-    // Reset input fields after search
-    setDepartureStation("");
-    setArrivalStation("");
-    setServiceNumber("");
+    // TODO: Logic for search
   };
 
   return (
     <View style={styles.container}>
-      <Switch
-        style={styles.switch}
-        value={searchType === 1}
-        onValueChange={toggleSearchType}
-      />
-      {searchType === 0 ? (
+      <View style={styles.switchContainer}>
+        <Text style={styles.label}>Numéro de Train</Text>
+        <Switch
+          style={styles.switch}
+          value={isModeSearchNumberTrain}
+          onValueChange={toggleModeSearch}
+        />
+      </View>
+      {isModeSearchNumberTrain ? (
         <View style={styles.searchSection}>
-          <CustomInput
-            label="Gare de départ"
-            placeholder="Entrez la gare de départ"
-            value={departureStation}
-            onTextChange={setDepartureStation}
-          />
-          <CustomInput
-            label="Gare d'arrivée"
-            placeholder="Entrez la gare d'arrivée"
-            value={arrivalStation}
-            onTextChange={setArrivalStation}
+          <TextInput
+            mode="outlined"
+            placeholder="Entrez le numéro de circulation"
+            label="Numéro de circulation"
+            onChangeText={setNumberCirculation}
+            value={numberCirculation}
+            keyboardType="number-pad"
           />
         </View>
       ) : (
         <View style={styles.searchSection}>
-          <CustomInput
-            label="Numéro de circulation"
-            placeholder="Entrez le numéro de circulation"
-            value={serviceNumber}
-            onTextChange={setServiceNumber}
+          <TextInput
+            mode="outlined"
+            label="Gare de départ"
+            placeholder="Entrez la gare de départ"
+            value={departureStation}
+            onChangeText={setDepartureStation}
+          />
+          <TextInput
+            mode="outlined"
+            label="Gare d'arrivée"
+            placeholder="Entrez la gare d'arrivée"
+            value={arrivalStation}
+            onChangeText={setArrivalStation}
           />
         </View>
       )}
@@ -87,25 +62,26 @@ const SearchTrain = () => {
       </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
   },
-  switch: {
-    alignSelf: "center",
+  switchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     marginBottom: 20,
+  },
+  switch: {
+    marginHorizontal: 10,
+  },
+  label: {
+    fontSize: 16,
   },
   searchSection: {
     marginBottom: 30,
   },
-
-  label: {
-    marginBottom: 4,
-    fontSize: 16,
-  },
 });
-
-export default SearchTrain;
