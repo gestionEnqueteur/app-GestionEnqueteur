c750dab345e488dd1541f6b3077acfb0bab1e616
Anothays <jeremy.snnk@gmail.com>
2024-06-07 20:15:51 +0200

    👔 Create useApi.tsx hook

--- hook/useApi.tsx
+++ hook/useApi.tsx
@@ -1,19 +1,15 @@
 import axios from 'axios';
 import { useRecoilValue } from 'recoil';
-import { configurationState, userState } from "../store/storeAtom";
+import { configurationState, jwtState } from "../store/storeAtom";
 
 export default function useApi() {
   const config = useRecoilValue(configurationState);
-  const user = useRecoilValue(userState);
+  const jwt = useRecoilValue(jwtState);
   const baseURL = config.urlApi;
-
-  const headers = user ? { Authorization: `Bearer ${user.token}` } : {};
-
+  const headers = jwt ? { Authorization: `Bearer ${jwt}` } : {};
   const customAxios = axios.create({
     baseURL,
     headers: headers
   });
-
-
   return customAxios;
 }
