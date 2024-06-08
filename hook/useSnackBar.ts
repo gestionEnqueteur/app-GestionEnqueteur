ca9e99170e64e4f9fa5b3145f55ab4802f6309d0
Rémi PONCHE <ponche62880@gmail.com>
2024-06-08 20:11:01 +0200

    📝 Ajout de la JsDoc du hook useSnackBar

--- hook/useSnackBar.ts
+++ hook/useSnackBar.ts
@@ -6,10 +6,21 @@ type CustomSnackBarProps = Omit<SnackbarProps, 'onDismiss' | 'visible'> & {
   onDismiss?: () => void;
 };
 
+/**
+ * ce hook permet de modifier les props du composant CommunSnackBar qui est au top level de l'application
+ * 
+ * @returns renvoie la fonction displaySnackbar
+ */
 export default function useSnackBar() {
 
   const setSnackBar = useSetRecoilState(snackBarState)
 
+  /**
+   * cette fonction permet d'afficher la CommunSnackBar pendant un brief instanst, elle recoit en argument une props SnackBarProps modifié.
+   * 
+   * @param snackBarProps Props modifié de SnackbarProps, voir : https://callstack.github.io/react-native-paper/docs/components/Snackbar
+   * ou l'attributs onDismiss est rendu facultative, et l'attribut visible, ne doit plus etre renseigné.
+   */
   const displaySnackBar = (snackBarProps: CustomSnackBarProps) => {
 
     const { onDismiss, ...restProps } = snackBarProps;
