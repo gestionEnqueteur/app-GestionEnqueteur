bb7bc01a0706fd45628b95df7f5296db17c74ffc
jeanCodeUnMax <webmanhtc@gmail.com>
2024-06-09 21:19:25 +0200

    :necktie: Ajout searchTrainScreen dans le router

--- screens/navigations/StackNavigation.tsx
+++ screens/navigations/StackNavigation.tsx
@@ -3,12 +3,14 @@ import { NavigatorScreenParams } from "@react-navigation/native";
 import HomeScreen from "../HomeScreen";
 import SaisieBscScreen from "../bsc/SaisieBscScreen";
 import MenuBottomTabs, { TabParamList } from "./MenuBottomTabs";
+import SearchTrainScreen from "../SearchTrainScreen";
 
 // type des param des pages
 export type RootStackParamList = {
   HomeStack: NavigatorScreenParams<TabParamList>;
   Test: undefined;
   SaisieBsc: { courseId: number };
+  SearchTrain: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -19,6 +21,7 @@ export default function StackNavigation() {
       <Stack.Screen name="HomeStack" component={MenuBottomTabs} />
       <Stack.Screen name="Test" component={HomeScreen} />
       <Stack.Screen name="SaisieBsc" component={SaisieBscScreen} />
+      <Stack.Screen name="SearchTrain" component={SearchTrainScreen} />
     </Stack.Navigator>
   );
 }
