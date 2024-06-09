9c4f3fda8cd7b9451636c1fb85e0fb12359b15b0
jeanCodeUnMax <webmanhtc@gmail.com>
2024-06-09 20:11:05 +0200

    :lipstick: ui de la page searchTrain

--- screens/SearchTrainScreen.tsx
+++ screens/SearchTrainScreen.tsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
-import { View, StyleSheet, Switch, Text } from "react-native";
-import { TextInput, Button } from "react-native-paper";
+import {
+  View,
+  StyleSheet,
+  Switch,
+  Text,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { TextInput, Button, IconButton } from "react-native-paper";
+import { DatePickerInput, TimePickerModal } from "react-native-paper-dates";
 
 export default function SearchTrainScreen() {
   const [departureStation, setDepartureStation] = useState("");
   const [arrivalStation, setArrivalStation] = useState("");
   const [numberCirculation, setNumberCirculation] = useState("");
   const [isModeSearchNumberTrain, setIsModeSearchNumberTrain] = useState(false);
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(
+    new Date()
+  );
+
+  const [isTimePickerVisible, setTimePickerVisible] = useState(false);
 
   const toggleModeSearch = () => {
     setIsModeSearchNumberTrain((prevMode) => !prevMode);
@@ -16,51 +29,118 @@ export default function SearchTrainScreen() {
     // TODO: Logic for search
   };
 
+  const onTimeDismiss = () => {
+    setTimePickerVisible(false);
+  };
+
+  const onTimeConfirm = ({
+    hours,
+    minutes,
+  }: {
+    hours: number;
+    minutes: number;
+  }) => {
+    let newDate = new Date();
+    if (selectedDate?.getDate()) {
+      newDate.setDate(selectedDate?.getDate());
+    }
+    newDate.setHours(hours);
+    newDate.setMinutes(minutes);
+    setSelectedDate(newDate);
+
+    setTimePickerVisible(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.switchContainer}>
-        <Text style={styles.label}>Numéro de Train</Text>
-        <Switch
-          style={styles.switch}
-          value={isModeSearchNumberTrain}
-          onValueChange={toggleModeSearch}
-        />
-      </View>
-      {isModeSearchNumberTrain ? (
-        <View style={styles.searchSection}>
-          <TextInput
-            mode="outlined"
-            placeholder="Entrez le numéro de circulation"
-            label="Numéro de circulation"
-            onChangeText={setNumberCirculation}
-            value={numberCirculation}
-            keyboardType="number-pad"
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <View style={styles.switchContainer}>
+          <Text style={styles.label}>Gare</Text>
+          <Switch
+            style={styles.switch}
+            value={isModeSearchNumberTrain}
+            onValueChange={toggleModeSearch}
+          />
+          <Text style={styles.label}>N° de Train</Text>
+        </View>
+        {isModeSearchNumberTrain ? (
+          <View style={styles.searchCirculation}>
+            <TextInput
+              mode="outlined"
+              placeholder="Entrez le numéro de circulation"
+              label="Numéro de circulation"
+              onChangeText={(newValue) => setNumberCirculation(newValue)}
+              value={numberCirculation}
+              keyboardType="number-pad"
+            />
+          </View>
+        ) : (
+          <View style={styles.searchSection}>
+            <TextInput
+              style={styles.inputDepartureStation}
+              mode="outlined"
+              label="Gare de départ"
+              placeholder="Entrez la gare de départ"
+              value={departureStation}
+              onChangeText={(newValue) => setDepartureStation(newValue)}
+            />
+            <TextInput
+              mode="outlined"
+              label="Gare d'arrivée"
+              placeholder="Entrez la gare d'arrivée"
+              value={arrivalStation}
+              onChangeText={(newValue) => setArrivalStation(newValue)}
+            />
+            <View style={styles.searchTime}>
+              <IconButton
+                style={styles.clockIcon}
+                onPress={() => setTimePickerVisible(true)}
+                icon="clock-outline"
+                size={50}
+              />
+              <Text>
+                {`${selectedDate?.getHours()}: ${selectedDate?.getMinutes()} `}
+              </Text>
+            </View>
+
+            <TimePickerModal
+              visible={isTimePickerVisible}
+              onDismiss={onTimeDismiss}
+              onConfirm={onTimeConfirm}
+              hours={selectedDate?.getHours()}
+              minutes={selectedDate?.getMinutes()}
+              label="Sélectionner l'heure"
+              cancelLabel="Annuler"
+              confirmLabel="Confirmer"
+              animationType="fade"
+            />
+          </View>
+        )}
+        <View style={styles.dateTimeContainer}>
+          <DatePickerInput
+            locale="fr"
+            label="Sélectionner Date"
+            value={selectedDate}
+            onChange={(newValue: Date | undefined) => {
+              let newDate = new Date();
+              if (selectedDate?.getTime()) {
+                newDate?.setTime(selectedDate?.getTime());
+              }
+              if (newValue?.getDate()) {
+                newDate?.setDate(newValue?.getDate());
+              }
+              setSelectedDate(newDate);
+            }}
+            inputMode="start"
           />
         </View>
-      ) : (
-        <View style={styles.searchSection}>
-          <TextInput
-            mode="outlined"
-            label="Gare de départ"
-            placeholder="Entrez la gare de départ"
-            value={departureStation}
-            onChangeText={setDepartureStation}
-          />
-          <TextInput
-            mode="outlined"
-            label="Gare d'arrivée"
-            placeholder="Entrez la gare d'arrivée"
-            value={arrivalStation}
-            onChangeText={setArrivalStation}
-          />
+        <View style={styles.submit}>
+          <Button mode="contained" onPress={handleSearch}>
+            Rechercher
+          </Button>
         </View>
-      )}
-      <View>
-        <Button mode="contained" onPress={handleSearch}>
-          Rechercher
-        </Button>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -73,15 +153,41 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    marginBottom: 20,
+    marginBottom: 30,
   },
   switch: {
-    marginHorizontal: 10,
+    marginHorizontal: 20,
   },
   label: {
     fontSize: 16,
   },
   searchSection: {
-    marginBottom: 30,
+    marginBottom: 10,
+  },
+  dateTimeContainer: {
+    alignItems: "center",
+  },
+  selectedTimeText: {
+    fontSize: 16,
+    marginLeft: 10,
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  inputDepartureStation: {
+    marginBottom: 15,
+  },
+  submit: {
+    marginTop: 50,
+  },
+  searchTime: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  clockIcon: {
+    marginRight: 10,
+  },
+  searchCirculation: {
+    marginBottom: 50,
   },
 });
