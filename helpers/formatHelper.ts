cd0535cbf7dc806da0efe31a9b450d9dbaf60767
jeanCodeUnMax <webmanhtc@gmail.com>
2024-06-12 00:00:57 +0200

    :recycle: Refacto de la fonction formatTime

--- helpers/formatHelper.ts
+++ helpers/formatHelper.ts
@@ -8,13 +8,29 @@ export function removeDashes(word: string): string {
 }
 
 /**
- * convert time string in string to hh:mm format
- * @param time time to convert en string
- * @returns the date in hh:mm format
+ * Convert time to hh:mm format
+ * @param times Time to convert, can be a Date object, a string, or an object with 'hours' and 'minutes' properties
+ * @returns The time in hh:mm format
  */
-export function formatTime(timestr: string): string {
-  const time = new Date(timestr); 
-  const hours = time.getHours().toString().padStart(2, "0");
-  const minutes = time.getMinutes().toString().padStart(2, "0");
-  return `${hours}:${minutes}`;
+export function formatTime(
+  times: Date | string | { hours: number; minutes: number }
+): string {
+  let hours: number;
+  let minutes: number;
+
+  if (typeof times === "object" && "hours" in times && "minutes" in times) {
+    // If times is an object with hours and minutes properties
+    ({ hours, minutes } = times);
+  } else {
+    const time = new Date(times);
+    if (isNaN(time.getTime())) {
+      throw new Error("Invalid input type for formatTime function");
+    }
+    hours = time.getHours();
+    minutes = time.getMinutes();
+  }
+
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}`;
 }
