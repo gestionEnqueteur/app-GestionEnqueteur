7d3d62a92420d62d976123fd6144a12c2ea81552
Rémi PONCHE <ponche62880@gmail.com>
2024-06-15 17:03:50 +0200

    ♻️ Modification du reducerCourse pour les Array
    modification du reducer Course afin de pourvoir mettre à jour le state avec un array.

--- reducer/courseReducer.ts
+++ reducer/courseReducer.ts
@@ -3,16 +3,16 @@ import Course from "../models/Course";
 export type ActionCourse =
   | { type: "update"; course: Course }
   | { type: "delete"; course: Course }
-  | { type: "add"; course: Course }
-  | { type: "load"; courses: Course[]}
-  | { type: "reset"}
+  | { type: "add"; course: Course | Course[] }
+  | { type: "load"; courses: Course[] }
+  | { type: "reset" };
 
 export default function courseReducer(state: Course[], action: ActionCourse) {
   let newState: Course[];
 
   switch (action.type) {
     case "add":
-      newState = [...state, action.course];
+      newState = addCourse(state, action.course);
       return newState;
 
     case "delete":
@@ -21,20 +21,42 @@ export default function courseReducer(state: Course[], action: ActionCourse) {
 
     case "update":
       newState = state.map((item) =>
-        item.id === action.course.id ? {...action.course, isSyncro: false} : item
+        item.id === action.course.id
+          ? { ...action.course, isSyncro: false }
+          : item
       );
-      console.log("action update"); 
+      console.log("action update");
       return newState;
 
-    case "load": 
+    case "load":
       newState = action.courses;
       return newState;
 
-    case "reset": 
+    case "reset":
       newState = [];
-      return newState; 
+      return newState;
 
     default:
       return state;
   }
+}
+
+// méthode interne pour le reducer
+function addCourse(prevState: Course[], courses: Course | Course[]) {
+  // on vérifie le type
+  if (courses instanceof Array) {
+    // c'est un array
+    const newListCourse: Course[] = [];
+    // vérification des doublon
+    for (const course of courses) {
+      if (prevState.find((item) => item.id === course.id) === undefined) {
+        newListCourse.push(course);
+      }
+    }
+    return [...prevState, ...newListCourse];
+  } else if (prevState.find((item) => item.id === courses.id) === undefined) {
+    return [...prevState, courses];
+  }
+  console.warn("duplication ID"); 
+  return prevState;
 }
